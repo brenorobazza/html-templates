ec25f3b1e30ebccfbb3f1320d3cb67ef9351d7f7
brenorobazza <breno.robazza@hotmail.com>
2020-10-04 16:52:28 -0300

    Added CSS and JS to code tabs and made preview area remain in position when scrolling

--- index.js
+++ index.js
@@ -4,40 +4,73 @@ script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
 
-var globalCM;
-var globalEditor;
+var globalHTMLEditor;
+var globalHTML;
+var globalReplacedHTML;
+var globalCSS;
+var globalJS;
 
-function showPreview(inputValue) {
-    // console.log(inputValue)
-    var htmlCode = inputValue;
-    // var cssCode = "";
-    // var jsCode = "" + document.getElementById("jsCode").value + "";
+// Show html preview in the left
+function showPreview() {
+    var htmlCode = globalHTML;
+    var cssCode = "<style>" + globalCSS + "</style>";
+    var jsCode = "<scr" + "ipt>" + globalJS + "</scr" + "ipt>";
     var frame = document.getElementById("preview-window").contentWindow.document;
     frame.open();
-    frame.write(htmlCode);
+    frame.write(htmlCode + cssCode + jsCode);
     frame.close();
 }
 
+// Load COdeMirror textareas
 window.addEventListener("load", function () {
-    var editor = CodeMirror.fromTextArea
+    // Code mirror for HTML
+    var editorHTML = CodeMirror.fromTextArea
         (document.getElementById("editor"), {
-            lineNumbers: true,
             mode: "xml",
             theme: "dracula",
             autoCloseTags: true,
-            onChange: function (cm) {
-                mySecondTextArea.value = cm.getValue();
-            },
+            autoCloseBrackets: true,
         });
-    editor.setSize("100%", "100%")
-    editor.on('change', function (cMirror) {
-        showPreview(cMirror.getValue());
-        globalCM = cMirror.getValue()
-    }
-    )
-    globalEditor = editor;
+    globalHTMLEditor = editorHTML;
+
+    // Code mirror for CSS
+    var editorCSS = CodeMirror.fromTextArea
+        (document.getElementById('editorCSS'), {
+            mode: "css",
+            theme: "dracula",
+            autoCloseTags: true,
+            autoCloseBrackets: true,
+        });
+
+    // Code mirror for JS
+    var editorJS = CodeMirror.fromTextArea
+        (document.getElementById('editorJS'), {
+            mode: "javascript",
+            theme: "dracula",
+            autoCloseTags: true,
+            autoCloseBrackets: true,
+        });
+
+    editorHTML.setSize(null, "100%");
+    editorCSS.setSize(null, "100%");
+    editorJS.setSize(null, "100%");
+
+    // When you edit, the preview is updated with the new value
+    editorHTML.on('change', function (cMirror) {
+        globalHTML = cMirror.getValue();
+        showPreview();
+    })
+    editorCSS.on('change', function (cMirror) {
+        globalCSS = cMirror.getValue();
+        showPreview();
+    })
+    editorJS.on('change', function (cMirror) {
+        globalJS = cMirror.getValue();
+        showPreview();
+    })
 })
 
+// Switch Code/Placeholders/Template tab
 function switchTab(tab) {
     var i;
     var x = document.getElementsByClassName("tab");
@@ -47,8 +80,19 @@ function switchTab(tab) {
     document.getElementById(tab).style.display = "block";
 }
 
+// Switch HTML/CSS/JS tabs
+function switchCodeTab(codeTab) {
+    var i;
+    var x = document.getElementsByClassName("codeTab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    document.getElementById(codeTab + "-tab").style.display = "block";
+}
+
+
 var originalHTML = document.getElementById('placeholders-table').innerHTML
-function addPlaceholders(placeholders) {// console.log("placeholders", placeholders)
+function addPlaceholders(placeholders) {
     var table = document.getElementById('placeholders-table')
 
     table.innerHTML = originalHTML
@@ -61,29 +105,28 @@ function addPlaceholders(placeholders) {// console.log("placeholders", placehold
     }
 }
 
+// generate the palceholders table
 function generateTable() {
-    var allPlaceholders = globalCM.match(/\{(.+?)\}/g)
+    // match everything that has curly brackets around
+    var allPlaceholders = globalHTML.match(/\{(.+?)\}/g)
     var placeholders = [];
     $.each(allPlaceholders, function (i, el) {
         if ($.inArray(el, placeholders) === -1) placeholders.push(el);
     });
-    // var placeholders = [...new Set(allPlaceholders)]
     addPlaceholders(placeholders)
 }
 
+// Replace all the placeholders for it's corresponding value
 function replaceValues() {
     var table = document.getElementById('placeholders-table');
 
     var find_and_replace = [];
     var find;
     var replace;
-    // LOOP THROUGH EACH ROW OF THE TABLE AFTER HEADER.
     for (i = 0; i < table.rows.length; i++) {
 
-        // GET THE CELLS COLLECTION OF THE CURRENT ROW.
         var objCells = table.rows.item(i).cells;
 
-        // LOOP THROUGH EACH CELL OF THE CURENT ROW TO READ CELL VALUES.
         for (var j = 0; j < objCells.length; j++) {
             if (j % 2) {
                 let value = objCells.item(j).innerHTML.match(/(?<=>)(.*)(?=<)/)[0]
@@ -91,8 +134,10 @@ function replaceValues() {
                     replace = value
                 }
                 else {
-
+                    // If not commented, the placeholder will remain if there is no value to replace it
                     replace = objCells.item(j - 1).innerHTML
+
+                    // If not commented, the placeholder will disappear if there is no value to replace it
                     // replace = ''
                 }
             }
@@ -100,7 +145,6 @@ function replaceValues() {
                 find = objCells.item(j).innerHTML
             }
 
-            // info.innerHTML = info.innerHTML + ' ' + objCells.item(j).innerHTML;
         }
         find_and_replace.push({
             key: find,
@@ -108,46 +152,33 @@ function replaceValues() {
         });
     }
     for (i in find_and_replace) {
-        // console.log(find_and_replace[1].key, find_and_replace[i].value)
-        globalCM = globalCM.replaceAll(find_and_replace[i].key, find_and_replace[i].value)
+        globalReplacedHTML = globalHTML.replaceAll(find_and_replace[i].key, find_and_replace[i].value)
     }
-    // console.log(find_and_replace)
-    setHTMLCode(globalCM, false)
+    setHTMLCode(globalReplacedHTMLHTML, false)
 }
 
 const fileSelector = document.getElementById('file-selector');
 fileSelector.addEventListener('change', (event) => {
     const fileList = event.target.files;
-    console.log(fileList['0']);
     readTemplate(fileList['0'])
 });
 
 function readTemplate(file) {
-    // Check if the file is an image.
     if (file.type && file.type.indexOf('html') === -1) {
-        console.log('File is not an html.', file.type, file);
+        alert('File is not an html.', file.type, file);
         return;
     }
-    
+
     const reader = new FileReader();
     reader.addEventListener('load', (event) => {
         setHTMLCode(event.target.result, true);
-        // console.log(event.target.result);
     });
     reader.readAsText(file);
 }
 
 function setHTMLCode(code, buildTable) {
-    console.log("code:", code)
-
-    // var editor = CodeMirror.fromTextArea(document.getElementById("editor"));
-    globalEditor.getDoc().setValue(code);
-
+    globalHTMLEditor.getDoc().setValue(code);
     if (buildTable) {
         generateTable();
     }
-    // editor.value=code;
 }
-
-
-watch(document.getElementById('placeholders-table'), replaceValues);
